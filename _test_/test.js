09e46f4a737e1ca7e27f8603d207036b57223ba8
Ashish Vashisht <concur.dev@gmail.com>
2025-03-03 00:27:58 +0100

    added dotenv as dependency

--- _test_/test.js
+++ _test_/test.js
@@ -5,11 +5,7 @@ const Avlogs = require("../avlogs");
 
 const avlogs = new Avlogs();
 
-console.log("\n--- Logging test started ---\n");
-
 avlogs.info("Application started successfully");
 avlogs.debug("Debugging mode active");
 avlogs.warn("Low memory warning");
 avlogs.error("Unhandled exception occurred");
-
-console.log("\n--- Logging test completed ---\n");
